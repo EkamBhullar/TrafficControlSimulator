857886bcfa3f6ec09f8935db1be7d8a902b1dc2d
Ekamjot Singh Bhullar <123894878+EkamBhullar@users.noreply.github.com>
2024-03-05 00:39:16 +0530

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -8,28 +8,23 @@
 #include <iomanip>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
-
 mutex dataMutex;
 
-// Initializing number of threads for producer and consumer
 int numProducerThreads = 3;
 int numConsumerThreads = 3;
 
-int producerCount = 0; // producer counter
-int consumerCount = 0; // consumer counter
-int totalRows = 0; // total number of rows in the data file
+int producerCount = 0; 
+int consumerCount = 0;
+int totalRows = 0; 
 
-condition_variable producerCV, consumerCV; // Initializing condition variables for producer and consumer
+condition_variable producerCV, consumerCV; 
 
-// String variables and vectors are initialized to get values from the data file
 vector<int> indexVector;
 vector<int> trafficLightVector;
 vector<int> carCountVector;
 vector<string> timeStampVector;
 
-// Struct for traffic data row
 struct TrafficSignal
 {
     int index;
@@ -40,11 +35,8 @@ struct TrafficSignal
 
 TrafficSignal signalArray[5] = {{0, "", 1, 0}, {0, "", 2, 0}, {0, "", 3, 0}, {0, "", 4, 0},{0, "", 5, 0}};
 
-// Queue to store traffic light data
+
 queue<TrafficSignal> trafficSignalQueue;
-
-
-// Function to sort traffic light data
 bool sortByTrafficCount(TrafficSignal first, TrafficSignal second)
 {
     return  (first.numberOfCars > second.numberOfCars);
@@ -55,12 +47,12 @@ void producerFunction()
 {
     while (producerCount < totalRows)
     {
-        unique_lock<mutex> lock(dataMutex); // Locking until producer finishes processing
+        unique_lock<mutex> lock(dataMutex); 
 
-        if (producerCount < totalRows) // If count is less than the number of rows in the dataset
+        if (producerCount < totalRows)
         {
             trafficSignalQueue.push(TrafficSignal{indexVector[producerCount], timeStampVector[producerCount], trafficLightVector[producerCount], carCountVector[producerCount]}); // Push into queue
-            consumerCV.notify_all(); // Notifying consumer threads
+            consumerCV.notify_all(); 
             producerCount++;
         }
         else
@@ -68,7 +60,7 @@ void producerFunction()
             producerCV.wait(lock, [] { return producerCount < totalRows; }); // If count is greater than the number of rows in the dataset, wait
         }
 
-        lock.unlock(); // Unlock after processing
+        lock.unlock();
         this_thread::sleep_for(chrono::seconds(rand() % 3));
     }
 }
@@ -81,9 +73,8 @@ void consumerFunction()
 
         if (!trafficSignalQueue.empty())
         {
-            signal = trafficSignalQueue.front(); // Getting the front elements of the queue
+            signal = trafficSignalQueue.front();
 
-            // Add the the number of cars into the respective traffic light id
             if (signal.trafficLightID == 1)
             {
                 signalArray[0].numberOfCars += signal.numberOfCars;
@@ -105,18 +96,18 @@ void consumerFunction()
                 signalArray[4].numberOfCars += signal.numberOfCars;
             }
 
-            trafficSignalQueue.pop(); // Pop the data
-            producerCV.notify_all(); // Notify producer
+            trafficSignalQueue.pop(); 
+            producerCV.notify_all(); 
             consumerCount++;
         }
         else
         {
-            consumerCV.wait(lock, [] { return !trafficSignalQueue.empty(); }); // If queue is empty, wait until producer produces
+            consumerCV.wait(lock, [] { return !trafficSignalQueue.empty(); }); 
         }
 
         if (!trafficSignalQueue.empty() && consumerCount % 60 == 0)
-        { // Check if an hour passed by, checking every 60th row
-            sort(signalArray, signalArray + 5, sortByTrafficCount); // Sorting data
+        { 
+            sort(signalArray, signalArray + 5, sortByTrafficCount); 
             cout << "Time: " << signal.timeStamp << endl;
             cout << "Traffic Light" << "\t" << "Number of Cars" << endl;
             for (int i = 0; i < 5; ++i)
@@ -134,7 +125,7 @@ void consumerFunction()
 int main()
 {
 
-   // readTrafficDataFromFile();
+  
     ifstream file;
 
     file.open("traffic_data.txt");
